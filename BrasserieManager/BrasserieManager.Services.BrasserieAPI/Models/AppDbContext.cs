53cf05c360c26acc4d61b8fb6d01af506b11280a
Tsy Hoarana <hoarana.rakotomalala@al-enterprise.com>
2023-10-26 12:54:02 +0200

    (Project): first commit project init

---
+++ BrasserieManager/BrasserieManager.Services.BrasserieAPI/Models/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BrasserieManager.Services.BrasserieAPI.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public AppDbContext()
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
---
+++ BrasserieManager/BrasserieManager.Services.GrossisteAPI/Models/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BrasserieManager.Services.GrossisteAPI.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public AppDbContext()
+        {
+
+        }
+    }
+}
